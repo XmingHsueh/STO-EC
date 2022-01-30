c7957c0829ca32a98685e291ddd0456784d02275
XmingHsueh <73691121+XmingHsueh@users.noreply.github.com>
2022-01-30 14:41:08 +0800

    Delete utils directory

--- utils/template functions/Quartic.m
+++
@@ -1,8 +0,0 @@
-function obj = Quartic(var,opt)
-%Quartic function with noise
-%   - var: design variable vector
-%   - opt: shift vector
-    w = 1:length(opt);
-    var = var-opt;
-    obj = sum(w.*var.^4)+rand;
-end
--- utils/template functions/Schwefel.m
+++
@@ -1,7 +0,0 @@
-function obj = Schwefel(var,opt)
-%Schwefel 2.2 function
-%   - var: design variable vector
-%   - opt: shift vector
-    var = var-opt;  
-    obj = sum(abs(var))+prod(abs(var));
-end
