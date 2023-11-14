d786e88e96cccba35717c90409c92c5858eb0a7b
XmingHsueh <73691121+XmingHsueh@users.noreply.github.com>
2023-11-14 23:17:04 +0800

    Update and rename what_to_transfer_opt_k1000.m to main_what_k1000.m

--- main_what_k1000.m
+++ main_what_k1000.m
@@ -4,41 +4,40 @@
 % ------------
 % Description:
 % ------------
-% This file is the entry point of experimentally comparing transferability
-% measurements in what to transfer.
+% This file is the entry point of experimentally comparing various transferability
+% metrics for prioritizing the transferability in what to transfer.
 %
 % ------------
 % Reference:
 % ------------
-% X. Xue, C. Yang, L. Feng, et al. "Does Optimized Solution Always Help? A
-% Comprehensive Investigation of Transfer Evolutionary Algorithms", Submitted for Peer Review.
+% X. Xue, C. Yang, L. Feng, et al. "Solution Transfer in Evolutionary Optimization: An 
+% Empirical Study on Sequential Transfer", Submitted for Peer Review.
 
 clc,clear
 warning off;
 task_families = {'Sphere','Ellipsoid','Schwefel','Quartic','Ackley','Rastrigin','Griewank','Levy'}; % eight task families
 transfer_scenarios = {'a','e'}; % intra-family and inter-family transfers
-xis = [0 0.7 1]; % the parameter xi that controls optimum coverage
-similarity_distributions = {'c','u','i','d'}; % four representative similarity distributions
+similarity_distributions = {'h1','h2','m1','m2','m3','m4','l1','l2'}; % eight similarity distributions
 k = 1000; % the number of previously-solved source tasks
-folder_problems = '.\benchmarks-k1000'; % the folder that stores the test problems
-specifications = [1 1 1 1 50 k; % STOP 1
-    2 2 1 2	25 k; % STOP 2
-    3 1 1 3	30 k; % STOP 3
-    4 2 1 4	50 k; % STOP 4
-    5 1 3 3	25 k; % STOP 5
-    6 2 3 2	50 k; % STOP 6
-    7 1 2 3	25 k; % STOP 7
-    8 2 2 4	30 k; % STOP 8
-    1 1 3 1	25 k; % STOP 9
-    6 2 2 1	30 k; % STOP 10
-    5 1 2 1	50 k; % STOP 11
-    2 2 3 1	50 k]; % STOP 12
+folder_problems = '.\benchmarks-ea-k1000'; % the folder that stores the test problems
+specifications = [1 1 1 50 k; % STOP 1
+    2 2 2 25 k; % STOP 2
+    3 1 2 30 k; % STOP 3
+    4 2 1 50 k; % STOP 4
+    5 1 3 25 k; % STOP 5
+    6 2 4 50 k; % STOP 6
+    7 1 5 25 k; % STOP 7
+    8 2 6 30 k; % STOP 8
+    1 1 7 25 k; % STOP 9
+    6 2 8 30 k; % STOP 10
+    5 1 8 50 k; % STOP 11
+    2 2 7 50 k]; % STOP 12
 num_problems = size(specifications,1);
 optimizer = 'ea'; % evolutionary optimizer
 popsize = 50; % population size
 FEsMax = 5000; % the number of function evaluations available
 runs = 30; % the number of independent runs
-opts_sesto.transfer_mode = 'What'; %{'What','When','How'};
+opts_sesto.transfer_mode = 'What'; % 'What', 'When', 'How'
 opts_sesto.metrics = {'N','Ra','H','M1','WD','OC','ROC','KLD','SA'}; % two baselines and seven transferability measurements
 opts_sesto.evaluators = {'N','Re','F-1','F-5','F-10','D-M','D-P','D-G'}; % two baselines and six transferability evaluators
 opts_sesto.adaptations = {'N','Rh','M1-Te','M1-Tr','M1-Tm','M1-M','M2-A','OC-L','OC-A','OC-K','OC-N','ROC-L','SA-L'}; % two baselines and eleven transferability enhancers
@@ -51,10 +50,13 @@ count = 0*num_problems*runs*length(algorithm_list);
 for n = 1:num_problems
     results_opt = struct;
     % import the STOP to be solved
-    stop_tbo = STOP('func_target',task_families{specifications(n,1)},'trans_sce',...
-        transfer_scenarios{specifications(n,2)},'xi',xis(specifications(n,3)),...
-        'sim_distribution',similarity_distributions{specifications(n,4)},'dim',...
-        specifications(n,5),'k',specifications(n,6),'mode','opt','folder_stops',folder_problems);
+    stop_tbo = STOP('func_target',task_families{specifications(n,1)}, ...
+        'trans_sce',transfer_scenarios{specifications(n,2)},...
+        'sim_distribution',similarity_distributions{specifications(n,3)}, ...
+        'dim',specifications(n,4),...
+        'k',specifications(n,5), ...
+        'mode','opt',...
+        'folder_stops',folder_problems);
     target_task = stop_tbo.target_problem;
     knowledge_base = stop_tbo.knowledge_base;
     problem.fnc = target_task.fnc;
@@ -67,7 +69,7 @@ for n = 1:num_problems
             % parameter configurations of the TEA search engine
             opts_sesto.algorithm_id = algorithm_list(m);
             opts_sesto.knowledge_base = knowledge_base;
-            [solutions,objs] = tea_search(problem,popsize,FEsMax,...
+            [solutions,objs] = transfer_evolutionary_search(problem,popsize,FEsMax,...
                 optimizer,opts_sesto);
             solutions_all(:,m) = solutions;
             objs_all(:,m) = objs;
@@ -80,8 +82,7 @@ for n = 1:num_problems
     end
     % save the results
     save(['.\experimental studies\results-what-opt-k1000\',task_families{specifications(n,1)},'-T',...
-        transfer_scenarios{specifications(n,2)},'-xi',num2str(xis(specifications(n,3))),...
-        '-S',similarity_distributions{specifications(n,4)},'-d',num2str(specifications(n,5)),...
-        '-k',num2str(specifications(n,6)),'-what.mat'],'results_opt');
+        transfer_scenarios{specifications(n,2)},'-S',similarity_distributions{specifications(n,3)},...
+        '-d',num2str(specifications(n,4)),'-k',num2str(specifications(n,5)),'-what.mat'],'results_opt');
 end
 close(h);
