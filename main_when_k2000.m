27290560eb60ecea57239875f55f8becfe13462b
XmingHsueh <73691121+XmingHsueh@users.noreply.github.com>
2023-11-14 23:33:52 +0800

    .

--- main_when_k2000.m
+++ main_when_k2000.m
@@ -18,7 +18,7 @@ warning off;
 task_families = {'Sphere','Ellipsoid','Schwefel','Quartic','Ackley','Rastrigin','Griewank','Levy'}; % eight task families
 transfer_scenarios = {'a','e'}; % intra-family and inter-family transfers
 similarity_distributions = {'h1','h2','m1','m2','m3','m4','l1','l2'}; % eight similarity distributions
-k = 2000; % the number of previously-solved source tasks
+k = 2000; % the number of source tasks
 folder_problems = '.\benchmarks-ea-k2000'; % the folder that stores the test problems in scenario 2 (k=2000)
 specifications = [1 1 1 50 k; % STOP 1
     2 2 2 25 k; % STOP 2
