b47d2d0851bd2ab0af0360730f00599620518611
XmingHsueh <73691121+XmingHsueh@users.noreply.github.com>
2023-11-14 23:39:04 +0800

    .

--- utils/others/fit_relax.m
+++ utils/others/fit_relax.m
@@ -9,5 +9,6 @@ for i = 1:num_classes-1
     range = (i-1)*group_less+1:i*group_less;
     fit_ranks(ind(range)) = class_labels(i);
 end
+
 range = (num_classes-1)*group_less+1:num_samples;
 fit_ranks(ind(range)) = class_labels(num_classes);
