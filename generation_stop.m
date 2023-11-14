3a1cc857bd590ae3371e8789c2146489b96bc3d6
XmingHsueh <73691121+XmingHsueh@users.noreply.github.com>
2023-11-14 23:13:17 +0800

    Update generation_stop.m

--- generation_stop.m
+++ generation_stop.m
@@ -10,39 +10,49 @@
 % ------------
 % Reference:
 % ------------
-% X. Xue, C. Yang, L. Feng, et al. "A Scalable Test Problem Generator for Sequential Transfer Optimization", arXiv preprint, 2023.
+% X. Xue, C. Yang, L. Feng, et al. "A Scalable Test Problem Generator for Sequential 
+% Transfer Optimization", arXiv preprint, 2023.
 
 clc,clear
+warning off;
+
+%% Specify the STOPs to be generated
 task_families = {'Sphere','Ellipsoid','Schwefel','Quartic','Ackley','Rastrigin','Griewank','Levy'}; % eight task families
 transfer_scenarios = {'a','e'}; % intra-family and inter-family transfers
-xis = [0 0.7 1]; % the parameter xi that controls the optimum coverage of a box-constrained image
-similarity_distributions = {'c','u','i','d'}; % four representative similarity distributions
-k = 100; % the number of previously-solved source tasks
-no_problems = 12; % the number of individual benchmark problems
-folder_problems = '.\benchmarks-k100';
+similarity_distributions = {'h1','h2','m1','m2','m3','m4','l1','l2'}; % eight similarity distributions
+k = 2000; % the number of source tasks
+specifications = [1 1 1 50 k; % STOP 1
+    2 2 2 25 k; % STOP 2
+    3 1 2 30 k; % STOP 3
+    4 2 1 50 k; % STOP 4
+    5 1 3 25 k; % STOP 5
+    6 2 4 50 k; % STOP 6
+    7 1 5 25 k; % STOP 7
+    8 2 6 30 k; % STOP 8
+    1 1 7 25 k; % STOP 9
+    6 2 8 30 k; % STOP 10
+    5 1 8 50 k; % STOP 11
+    2 2 7 50 k]; % STOP 12
+backbone_opt = 'ea'; % ea, de_b, de_r
+FEsMax = 5000; % the maximum function evaluations
+no_problems = size(specifications,1);
+
+%% Generation of the STOPs
+folder_problems = ['.\benchmarks-',backbone_opt,'-k',num2str(k)]; % the folder used for storing the generated STOPs
 if ~isfolder(folder_problems)
     mkdir(folder_problems);
 end
-
-specifications = [1 1 1 1 50 k; % STOP 1
-    2 2 1 2	25 k; % STOP 2
-    3 1 1 3	30 k; % STOP 3
-    4 2 1 4	50 k; % STOP 4
-    5 1 3 3	25 k; % STOP 5
-    6 2 3 2	50 k; % STOP 6
-    7 1 2 3	25 k; % STOP 7
-    8 2 2 4	30 k; % STOP 8
-    1 1 3 1	25 k; % STOP 9
-    6 2 2 1	30 k; % STOP 10
-    5 1 2 1	50 k; % STOP 11
-    2 2 3 1	50 k]; % STOP 12
 count = 0; % the number of available STOPs
-
 for n = 1:no_problems
-    STOP('func_target',task_families{specifications(n,1)},'trans_sce',...
-        transfer_scenarios{specifications(n,2)},'xi',xis(specifications(n,3)),'sim_distribution',...
-        similarity_distributions{specifications(n,4)},'dim',specifications(n,5),'k',...
-        specifications(n,6),'mode','gen','folder_stops',folder_problems);
+    STOP('func_target',task_families{specifications(n,1)},...
+        'trans_sce',transfer_scenarios{specifications(n,2)},...
+        'sim_distribution',similarity_distributions{specifications(n,3)},...
+        'dim',specifications(n,4),...
+        'k',specifications(n,5),...
+        'optimizer',backbone_opt,...
+        'FEsMax',FEsMax,...
+        'mode','generation',...
+        'folder_stops',folder_problems);
     count = count+1;
     fprintf('#%d of the 12 problems is ready!\n',count);
 end
